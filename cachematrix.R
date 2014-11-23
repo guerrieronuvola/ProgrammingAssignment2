6155bdb2cd80095e31eeac570e2d1ff8f462a469
guerrieronuvola <terrytop@gmail.com>
2014-11-23 23:37:20 +0100

    cacheSolve() : "if" structure for checking cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## just testing
-
-## Write a short comment describing this function
+## This function will cache potentially time-consuming matrix invertion.
+## When called, it will create a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+
+## This function will check for any cached matrix invertion.
+## If the inverse matrix has already been calculated, it will be retrieved
+## from the cache, otherwise it will be calculated.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	i <- xget$getinv()
+	if (!is.null(i)) {
+		print("Getting Inverse Matrix from cached data..."
+		return (i)
+	}
+## Fetch the inverse matrix and verify if it's null: if not, return the cached matrix
+
+	data <- x$get()
+	i <- solve(data, ...)
+	x$setinv(i)
+	i
+## Calculate the inverse matrix and return it
 }
